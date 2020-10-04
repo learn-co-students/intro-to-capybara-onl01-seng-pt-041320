dc2281d0c1d619dc3529b34ea6094162cd6f9e68
eulis01 <eulis01@gmail.com>
2020-10-04 01:35:46 -0400

    added comment to code

--- app.rb
+++ app.rb
@@ -1,5 +1,6 @@
+# 1-> we are goin to ask the Application class to to use the Sinatra::Base classes
 class Application < Sinatra::Base
-
+# 2-> configuring basic route for the views
   get "/" do 
     erb :index
   end
